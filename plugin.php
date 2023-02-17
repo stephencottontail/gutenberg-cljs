314d5a0bbb34dd3361516d9b7523f70ccbfc018b
Stephen Dickinson <stephencottontail@me.com>
2023-02-17 11:10:06 -0700

    WIP: get registerBlockType to work

--- plugin.php
+++ plugin.php
@@ -5,6 +5,22 @@
  * Version: 0.1.0
  */
 
-if ( ! defined( 'WP_INC' ) ) {
-	die;
+if ( ! defined( 'WPINC' ) ) {
+    die;
 }
+/*
+add_action( 'block_categories_all', function( $categories ) {
+    return array_merge( $categories, array(
+	'title' => 'ClojureScript',
+	'slug'  => 'cljs'
+    ) );
+} );
+*/
+add_action( 'init', function() {
+    wp_register_script( 'cljs-embed', plugin_dir_url( __FILE__ ) . '/resources/public/js/main.js', array( 'react', 'wp-blocks' ), null, true );
+    //wp_register_script( 'cljs-embed', plugin_dir_url( __FILE__ ) . '/target/public/cljs-out/dev/main_bundle.js', array( 'wp-blocks' ), null, true );
+    
+    register_block_type( 'cljs/embed', array(
+	'editor_script' => 'cljs-embed',
+    ) );
+} );
